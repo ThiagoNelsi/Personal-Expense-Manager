1a40e42318ae655b27e0241760a00d3864bf2784
Thiago Nelsi do Couto <thiagonelsi@yahoo.com.br>
2020-02-25 22:39:52 +0000

    Add autoFocus to moneyInput and auto set '00' value to cents input

--- mobile/src/pages/NewExpence/index.js
+++ mobile/src/pages/NewExpence/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, KeyboardAvoidingView, Text, TextInput, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
 function NewExpence() {
+
+  const [cents, setCents] = useState('00');
+
   return (
     <KeyboardAvoidingView style={styles.container} behavior='padding'>
       <View style={styles.subContainer}>
         <View style={styles.moneyInputContainer}>
           <Text style={styles.moneyInputText}>R$</Text>
           <TextInput style={styles.moneyInput} keyboardType='decimal-pad' maxLength={5} autoFocus={true} />
-          <TextInput style={styles.centsInput} keyboardType='decimal-pad' maxLength={2} />
+          <TextInput style={styles.centsInput} keyboardType='decimal-pad' maxLength={2} value={cents} onChangeText={text => setCents(text)} />
         </View>
         <View>
           <TextInput style={styles.descriptionInput} placeholder='Descrição...' maxLength={35} />
